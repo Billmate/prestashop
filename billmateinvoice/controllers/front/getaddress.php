9fa239daf2c167f59ed977dbb8960e7d87b22adb
Andy Johansson <andy.johansson@me.com>
2014-10-20 17:52:40 +0200

    Changed flag for shipping fee
    Old flag was 16, the sum was then handeld as service fee. New should flag should be 8 as it will be set as shipping fee excluding VAT.

--- billmateinvoice/controllers/front/getaddress.php
+++ billmateinvoice/controllers/front/getaddress.php
@@ -532,7 +532,7 @@ class BillmateInvoiceGetaddressModuleFrontController extends ModuleFrontControll
 						'price'    => (int)($shippingPrice*100),
 						'vat'      => (float)$taxrate,
 						'discount' => 0.0,
-						'flags'    => 16, //16|32
+						'flags'    => 8,
 					)
 				);
 			}
