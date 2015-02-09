280f9cc66fb19387c8813fb725b396b9ad3073b3
Jesper Johansson <jesper@boxedlogistics.se>
2015-02-09 12:44:04 +0100

    Layout improvment. Added 10px to the height to prevent radio buttons outside container
    Fixes #91

--- billmatecardpay/js/billmate.js
+++ billmatecardpay/js/billmate.js
@@ -15,5 +15,5 @@ $(document).ready(function(){
 	    height = $(this).height();
     });
 
-    $('.billmate-blockSmall').css({'height' : $('.billmate-blockSmall').css('height', height+'px')});
+    $('.billmate-blockSmall').css({'height' : $('.billmate-blockSmall').css('height', 10 +height+'px')});
 });
