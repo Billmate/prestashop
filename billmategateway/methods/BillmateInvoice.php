6f197a6f436105a61a047dbcd9b9d765c4ad9149
Jesper Johansson <jesper@boxedlogistics.se>
2015-04-13 12:09:59 +0200

    Removed the space in link
    Fixes #34

--- billmategateway/methods/BillmateInvoice.php
+++ billmategateway/methods/BillmateInvoice.php
@@ -53,7 +53,7 @@
 				'type'       => $this->name,
 				'controller' => $this->validation_controller,
 				'icon'       => $this->icon,
-				'agreements' => sprintf($this->l('My email is accurate and can be used for invoicing.').'<a id="terms" style="cursor:pointer!important"> '.$this->l('I confirm the terms for invoice payment').'</a>'),
+				'agreements' => sprintf($this->l('My email is accurate and can be used for invoicing.').' <a id="terms" style="cursor:pointer!important">'.$this->l('I confirm the terms for invoice payment').'</a>'),
 				'invoiceFee' => $this->getFee()
 			);
 		}
