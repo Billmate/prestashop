47ca946ec2927f5fcf6df35906521f7af81d900c
Paul Herrström <kalorifri@gmail.com>
2017-11-24 16:21:08 +0100

    Override LinkCore to link to Billmate Checkout

---
+++ billmategateway/override/classes/Link.php
@@ -0,0 +1,15 @@
+<?php
+
+class Link extends LinkCore
+{
+    public function getPageLink($controller, $ssl = null, $id_lang = null, $request = null, $request_url_encode = false, $id_shop = null, $relative_protocol = false)
+    {
+        if ($controller == 'order-opc' AND Configuration::get('BILLMATE_CHECKOUT_ACTIVATE') == 1) {
+            $return = $this->getBaseLink($id_shop, $ssl, $relative_protocol).'module/billmategateway/billmatecheckout';
+        } else {
+            $return = parent::getPageLink($controller, $ssl, $id_lang, $request, $request_url_encode, $id_shop, $relative_protocol);
+        }
+        return $return;
+    }
+}
+?>
