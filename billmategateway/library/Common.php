64b98fd4562a8ab1585fcfe2b89025c438fe6079
Andy Johansson <andy@andyjohansson.com>
2019-03-20 11:18:31 +0100

    Updated version number to 3.3.4

--- billmategateway/library/Common.php
+++ billmategateway/library/Common.php
@@ -14,7 +14,7 @@
 	require_once 'Billmate.php';
 	require_once 'Encoding.php';
 	require_once 'Utf8.php';
-    define('BILLMATE_PLUGIN_VERSION','3.3.3');
+    define('BILLMATE_PLUGIN_VERSION','3.3.4');
 	class Common {
 
 		public static function getBillmate($eid, $secret, $testmode, $ssl = true, $debug = false)
