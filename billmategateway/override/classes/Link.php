57174b8527df545cef18cf6580d8b6a1c0b4cc36
henrik-ecomatic <henrik@ecomatic.se>
2019-12-19 11:23:19 +0100

    PS-27 [BUGFIX] added check to see if user is on order page and not redirecting to billmate checkout if user is on order page

--- billmategateway/override/classes/Link.php
+++ billmategateway/override/classes/Link.php
@@ -4,10 +4,11 @@ class Link extends LinkCore
 
     public function getPageLink($controller, $ssl = null, $id_lang = null, $request = null, $request_url_encode = false, $id_shop = null, $relative_protocol = false)
     {
-        if ($this->isBMCheckoutEnabled($controller)) {
-            return $this->getModuleLink('billmategateway', 'billmatecheckout');
+        if (strpos($_SERVER['REQUEST_URI'], 'order') == false) {
+            if ($this->isBMCheckoutEnabled($controller)) {
+                return $this->getModuleLink('billmategateway', 'billmatecheckout');
+            }
         }
-
         return parent::getPageLink($controller, $ssl, $id_lang, $request, $request_url_encode, $id_shop, $relative_protocol);
     }
 
