6381f0c2fb1ad5059c7cbdbb50758fc2bd67dbb6
Stefan Lidén <stefanliden123@hotmail.com>
2020-01-13 08:54:54 +0100

    Update Common.php

--- billmategateway/library/Common.php
+++ billmategateway/library/Common.php
@@ -14,7 +14,7 @@
 	require_once 'Billmate.php';
 	require_once 'Encoding.php';
 	require_once 'Utf8.php';
-        define('BILLMATE_PLUGIN_VERSION','3.5.0');
+        define('BILLMATE_PLUGIN_VERSION','3.5.1');
 	class Common {
 
 		public static function getBillmate($eid, $secret, $testmode, $ssl = true, $debug = false)
