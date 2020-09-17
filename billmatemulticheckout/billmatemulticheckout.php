09b9e0cdd3859b9f23fc05d359b6b2cc396a93fa
Mattias Nilsson <mattias.nilsson@billmate.se>
2020-09-17 12:00:33 +0200

    [TASK] Add changes to billmatemulticheckout.php

--- billmatemulticheckout/billmatemulticheckout.php
+++ billmatemulticheckout/billmatemulticheckout.php
@@ -1,125 +1,116 @@
 <?php
 
-/**
- * 2017 Keshva Thakur
- * @author Keshva Thakur
- * @copyright Keshva Thakur
- * @license   https://www.prestashop.com/en/osl-license
- * @version   3.1.5
- */
-if (!defined('_PS_VERSION_'))
+if (!defined('_PS_VERSION_')) {
     exit;
+}
 
+/**
+ * Multi store Checkout for Billmate
+ */
 class billmatemulticheckout extends Module {
 
+    /**
+     * Constructor
+     */
     public function __construct() {
-        $this->name = 'billmatemulticheckout';
-        $this->description = 'billmatemulticheckout.';
-        $this->tab = 'pricing_promotion';
-        $this->version = '3.1.5';
-        $this->author = 'Per Ledin ';
-        $this->need_instance = 0;
-        $this->ps_versions_compliancy = array('min' => '1.5', 'max' => _PS_VERSION_);
-        $this->bootstrap = true;
-        $this->module_key = '';
-        $this->_html = '';
-        $this->unique_string = '';
-        $this->valid = '';
-        $this->simple_content_files_location = $this->_path . 'views/';
-        $this->ignore_changes_content_changes = false;
-
         parent::__construct();
 
+        $this->name         = 'billmatemulticheckout';
+        $this->moduleName   = 'billmatemulticheckout';
         $this->displayName = $this->l('Billmate multi store checkout enable');
         $this->description = $this->l('This module billmate multi store billmate checkout enable.');
         $this->confirmUninstall = $this->l('Are you sure you want to uninstall?');
+
+        $this->version      = BILLMATE_PLUGIN_VERSION;
+        $this->author       = 'Billmate AB';
+        $this->page         = basename(__FILE__, '.php');
+
+        $this->context->smarty->assign('base_dir', __PS_BASE_URI__);
+        $this->ps_versions_compliancy = array('min' => '1.5', 'max' => _PS_VERSION_);
+
+        $this->bootstrap = true;
+        $this->need_instance = 0;
+        $this->simple_content_files_location = $this->_path . 'views/';
+        $this->ignore_changes_content_changes = false;
     }
 
+    /**
+     * Install process
+     *
+     * @return bool
+     */
     public function install() {
-        if (Shop::isFeatureActive())
+        if (Shop::isFeatureActive()) {
             Shop::setContext(Shop::CONTEXT_ALL);
-        if (!parent::install() || !$this->registerHook('header') || !$this->registerHook('displayHome') || !$this->registerHook('footer'))
+        }
+        if (!parent::install() || !$this->registerHook('header') || !$this->registerHook('displayHome') || !$this->registerHook('footer')) {
             return true;
+        }
     }
 
+    /**
+     * Uninstall process
+     *
+     * @return bool
+     */
     public function uninstall() {
-
         if (!parent::uninstall()) {
             return true;
         }
         return true;
     }
 
+    /**
+     * Get the content
+     *
+     * @return string
+     */
     public function getContent() {
         $this->processSubmit();
         return $this->displayForm();
     }
-	
-	
-	
+
+    /**
+     * Process the submit action
+     *
+     * @retrun void
+     */
     public function processSubmit() {
-
-        $currency_id = "";
-        $currencyc = "";
+        $currency_id = '';
+        $currencyc = '';
         if (Tools::isSubmit('submit' . $this->name)) {
-
-           
-			$billmatemulticheckout_=Tools::getValue('billmatemulticheckout_');
-			
-			//echo "<pre>";
-			//print_r($billmatemulticheckout_);
-			//exit;
-		
-
-            foreach ($billmatemulticheckout_ as $keye => $value) {
-
-                                
-                $currency_id =$keye;
-
-                if ($value == '1' && $currency_id != "") {					
-                    
-					Configuration::updateValue('billmatemulticheckout_'.$keye, $value);
+            $billmatemulticheckout_ = Tools::getValue('billmatemulticheckout_');
+            foreach ($billmatemulticheckout_ as $key => $value) {
+                $currency_id = $key;
+                if ($value == '1' && $currency_id != '') {
+                    Configuration::updateValue('billmatemulticheckout_' . $key, $value);
                 }
-                if ($value == '0' && $currency_id != "") {
-
-                    
-					Configuration::updateValue('billmatemulticheckout_'.$keye, $value);
-
+                if ($value == '0' && $currency_id != '') {
+                    Configuration::updateValue('billmatemulticheckout_' . $key, $value);
                     $this->_html .= $this->displayConfirmation(" Remove product prices decimals disable sucessfully");
                 }
             }
-          //  exit;
         }
     }
 
+    /**
+     * Display the form
+     *
+     * @return string
+     */
     public function displayForm() {
         $fields_form = array();
+        $id_shop = (int)Context::getContext()->shop->id;
+        $shops_list = array();
+        $shops = Shop::getShops();
+        foreach ($shops as $shop){
+            $shops_list[] = array( 'id_shop' => $shop['id_shop'], 'name' => $shop['name'] );
+        }
 
-        $id_shop = (int) Context::getContext()->shop->id;
-
-
-	
-		$shops_list = array();
-		$shops = Shop::getShops();
-		foreach ($shops as $shop){
-			$shops_list[] = array( 'id_shop' => $shop['id_shop'], 'name' => $shop['name'] );
-	    }
-		
-			
-		
-		 foreach ($shops_list as $keye => $valuec) {
-			 
-			 
-			
-			 $valuec['billmatemulticheckout_'] = Configuration::get('billmatemulticheckout_'.$valuec['id_shop']);
-			 $allcurrenciesc[]=$valuec;
-			 
-		 }
-		
-
-         //echo "<pre>";
-         //print_r($allcurrenciesc);
-         //exit;
+        foreach ($shops_list as $key => $value) {
+             $value['billmatemulticheckout_'] = Configuration::get('billmatemulticheckout_' . $value['id_shop']);
+             $allcurrenciesc[] = $value;
+        }
 
         $fields_form[]['form'] = array(
             'input' => array(
@@ -132,7 +123,6 @@ class billmatemulticheckout extends Module {
             ),
         );
 
-
         $helper = new HelperForm();
         $helper->module = $this;
         $helper->name_controller = $this->name;
@@ -140,7 +130,4 @@ class billmatemulticheckout extends Module {
         $helper->currentIndex = AdminController::$currentIndex . '&configure=' . $this->name;
         return $this->_html . $helper->generateForm($fields_form);
     }
-
 }
-
-?>
