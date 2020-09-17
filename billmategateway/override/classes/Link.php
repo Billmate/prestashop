95ca237229ce0273739072b29f947c3043683de2
Mattias Nilsson <mattias.nilsson@billmate.se>
2020-09-17 11:29:03 +0200

    [TASK] Fix issues in Link.php

--- billmategateway/override/classes/Link.php
+++ billmategateway/override/classes/Link.php
@@ -1,28 +1,37 @@
 <?php
+
+/**
+ * Class Link extends LinkCore
+ */
 class Link extends LinkCore
 {
 
+    /**
+     * @param $controller
+     * @param null $ssl
+     * @param null $id_lang
+     * @param null $request
+     * @param false $request_url_encode
+     * @param null $id_shop
+     * @param false $relative_protocol
+     * @return mixed
+     */
     public function getPageLink($controller, $ssl = null, $id_lang = null, $request = null, $request_url_encode = false, $id_shop = null, $relative_protocol = false)
     {
-		
-				
         if (strpos($_SERVER['REQUEST_URI'], 'order') == false) {
-			
             if ($this->isBMCheckoutEnabled($controller)) {
-                return $this->getModuleLink('billmategateway', 'billmatecheckout');				
+                return $this->getModuleLink('billmategateway', 'billmatecheckout');
             }
-			
-			 $id_shop = (int)Context::getContext()->shop->id; 
-		     $controller_name = Tools::getValue('controller');
-			 $enable_billmate_other_store = Configuration::get('billmatemulticheckout_'.$id_shop);	
-		  		 
-			 if ($id_shop==$enable_billmate_other_store && $controller_name=="billmatecheckout") {	
-			 return $this->getModuleLink('billmategateway', 'billmatecheckout');
-			}			 
+
+            $id_shop = (int)Context::getContext()->shop->id;
+            $controller_name = Tools::getValue('controller');
+            $enable_billmate_other_store = Configuration::get('billmatemulticheckout_' . $id_shop);
+
+            if ($id_shop == $enable_billmate_other_store && $controller_name == "billmatecheckout") {
+                return $this->getModuleLink('billmategateway', 'billmatecheckout');
+            }
         }
-		
-		
-		
+
         return parent::getPageLink($controller, $ssl, $id_lang, $request, $request_url_encode, $id_shop, $relative_protocol);
     }
 
@@ -40,9 +49,7 @@ class Link extends LinkCore
         return false;
     }
 
-	
-	
-	/**
+    /**
      * @param $controller
      *
      * @return bool
@@ -50,7 +57,5 @@ class Link extends LinkCore
       protected function getBmModule()
     {
         return Module::getInstanceByName('billmategateway');
-    } 
-	
-	
+    }
 }
