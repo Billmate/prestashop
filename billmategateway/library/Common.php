26692341c5794a10e5e75ccf75d690c731d21b15
David Villa <david@kfoobar.se>
2021-03-04 15:17:50 +0100

    [TASK] Prepare for release ''3.8.1''

--- billmategateway/library/Common.php
+++ billmategateway/library/Common.php
@@ -2,7 +2,7 @@
 	require_once 'Billmate.php';
 	require_once 'Encoding.php';
 	require_once 'Utf8.php';
-    define('BILLMATE_PLUGIN_VERSION', '3.8.0');
+    define('BILLMATE_PLUGIN_VERSION', '3.8.1');
 
     /**
      * Class for Common Billmate related tasks
